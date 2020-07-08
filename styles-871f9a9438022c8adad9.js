9fb758e0659e94248bbf49175bcdc57a7e1d6dea
gautheron <arthur.gautheron@creatis.insa-lyon.fr>
2020-07-08 14:58:46 +0200

    Add Links to covid on each page + records of each voice

--- styles-871f9a9438022c8adad9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{565:function(n,w,o){}}]);
-//# sourceMappingURL=styles-871f9a9438022c8adad9.js.map
---
+++ styles-9ceb9824a8884cd61cea.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{566:function(n,w,o){}}]);
+//# sourceMappingURL=styles-9ceb9824a8884cd61cea.js.map
