69af36b205e7e30e035ca0fe1e441e4233e3757d
Arthur Gautheron <arthur.gautheron@gmail.com>
2026-03-12 10:07:03 +0100

    correct issues with hamburger button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   var socialLink = document.getElementById("list-social-links");
   var menuLink = document.getElementById("menu-list");
+	const buttonFollow = document.getElementById("button-follow");
 
-  document.getElementById("button-follow").addEventListener("click", function () {
-    if ((socialLink.style.display === "none") || (window.getComputedStyle(socialLink, null).display === "none")) {
-      socialLink.style.display = "block";
-    } else {
-      socialLink.style.display = "none";
-    }
-  });
-
+	if (buttonFollow) {
+	  buttonFollow.addEventListener("click", function () {
+		if (
+		  socialLink.style.display === "none" ||
+		  window.getComputedStyle(socialLink, null).display === "none"
+		) {
+		  socialLink.style.display = "block";
+		} else {
+		  socialLink.style.display = "none";
+		}
+	  });
+	}
   document.getElementById("button-menu").addEventListener("click", function() {
     if ((menuLink.style.display === "none") || (window.getComputedStyle(menuLink, null).display === "none")) {
       menuLink.style.display = "block";
